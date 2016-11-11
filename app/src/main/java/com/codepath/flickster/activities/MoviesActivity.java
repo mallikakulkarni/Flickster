6d81e8f0ac28ebea1ea9e17e3b4a42780631ef72
Mallika <mallikaa@yahoo-inc.com>
2016-11-11 14:58:19 -0800

    Pull to refresh complete

--- app/src/main/java/com/codepath/flickster/activities/MoviesActivity.java
+++ app/src/main/java/com/codepath/flickster/activities/MoviesActivity.java
@@ -2,6 +2,7 @@ package com.codepath.flickster.activities;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -30,18 +31,19 @@ public class MoviesActivity extends AppCompatActivity {
     MovieArrayAdapter movieArrayAdapter;
     private ActivityMoviesBinding binding;
     ListView lvItems;
+    private SwipeRefreshLayout swipeContainer;
+    private final String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movies);
         lvItems = binding.lvMovies;
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.activity_main);
         movies = new ArrayList<>();
         movieArrayAdapter = new MovieArrayAdapter(MoviesActivity.this, movies);
         lvItems.setAdapter(movieArrayAdapter);
         lvItems.setItemsCanFocus(true);
-
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
@@ -50,6 +52,7 @@ public class MoviesActivity extends AppCompatActivity {
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
             }
+
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (!response.isSuccessful()) {
@@ -70,5 +73,51 @@ public class MoviesActivity extends AppCompatActivity {
                 }
             }
         });
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    public void fetchTimelineAsync(int page) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Response invalid");
+                }
+                try {
+                    JSONObject jsonResponse = new JSONObject(response.body().string());
+                    JSONArray responseArray = jsonResponse.getJSONArray("results");
+                    movies = new ArrayList<Movie>();
+                    movies.addAll(Movie.getMovies(responseArray));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            movieArrayAdapter.notifyDataSetChanged();
+                            swipeContainer.setRefreshing(false);
+                        }
+                    });
+                } catch (JSONException jsone) {
+                    Log.d("debug", "Cannot get JSON Reponse");
+                }
+            }
+        });
+
     }
 }
